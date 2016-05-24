e7fc9895b7c47f8af8b693b9fd575499d0d142c4
Cristofer <Cristofer.dev@gmail.com>
2016-05-24 15:44:41 -0400

    Habilitada la Función Agregar
    En app.js se crea controlador y se define funcion agregar() que
    permite crear el array con las tareas.
    
    	modified:   js/app.js
    	modified:   views/tarea_agregar.html

--- js/app.js
+++ js/app.js
@@ -3,11 +3,23 @@ angular.module('taskApp',['ui.router'])
         $stateProvider
             .state('alta',{
                 url:'/alta',
-                templateUrl:'views/tarea_agregar.html'
+                templateUrl:'views/tarea_agregar.html',
+                controller: 'ControlAgregar'
             })            
             .state('editar',{
                 url:'/editar',
                 templateUrl:'views/tarea_editar.html'
             });
         $urlRouterProvider.otherwise('alta');
+    })
+    .controller('ControlAgregar', function($scope){
+        $scope.tarea = {};
+        $scope.tareas = [];
+
+        $scope.agregar = function(){
+            $scope.tareas.push({
+                nombre : $scope.tarea.nombre,
+                prioridad : parseInt($scope.tarea.prioridad)
+            })
+        }
     })
